0bb2142dd5018826c30f7ea837b58dbb67623fd7
gatsby93 <clement_ng93@hotmail.com>
2014-09-18 13:22:38 +0800

    GCM SUCCESSFUL!

--- GCMTest2/src/com/example/gcmtest2/MainActivity.java
+++ GCMTest2/src/com/example/gcmtest2/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.gcmtest2;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
+//import com.neel.jsonpost.pLocation;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -30,6 +41,7 @@ public class MainActivity extends Activity {
     private String regid = null;
     SharedPreferences prefs;
     private Context context= null;
+    private static final String URL ="http://172.19.10.119/locationservice/LocationService.svc/GetLocation";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -202,7 +214,58 @@ public class MainActivity extends Activity {
 	//IMPLEMENTATION OF SEND REGID
 	public void sendRegistrationIdToBackend()
 	{
+		InputStream inputStream = null;
+		String result ="";
+		//JSONObject json = data[0];
+		DefaultHttpClient hc = new DefaultHttpClient();
+		//HttpConnectionParams.setConnectionTimeout(hc.getParams(), 100000);
 		
+		JSONObject jsonResponse = null;
+		HttpPost httpPost = new HttpPost(URL);
+		try
+		{
+ 					
+			String json="";
+			JSONObject  jsonObject = new JSONObject();
+			jsonObject.accumulate("REGISTRATION_ID",regid);
+			json = jsonObject.toString();
+			
+			//System.out.println(json);
+			StringEntity se;
+			se = new StringEntity(json);
+			httpPost.setEntity(se);
+			
+			httpPost.setHeader("Accept","application/json");
+			httpPost.setHeader("Content-type","application/json");
+			//StringEntity entity = new StringEntity(json);
+			System.out.println("1");
+			//httpPost.setEntity(entity);				
+			System.out.println("2");
+		
+			HttpResponse response = (HttpResponse)hc.execute(httpPost);
+			System.out.println("3");
+
+			HttpEntity entity = response.getEntity();
+			System.out.println("4");
+			//inputStream = response.getEntity().getContent();
+			 
+			if(entity!=null)
+			{
+				System.out.println("5");
+				InputStream instream = entity.getContent();
+				result = convertInputStreamToString(instream);
+				instream.close();
+				System.out.println("6");	
+				System.out.println(result);
+				System.out.println(entity.toString());
+				//Toast.makeText(getBaseContext(), "Success", Toast.LENGTH_LONG).show();
+			}		
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		//return null;
 	}
 	
 	private void storeRegistrationId(Context context, String regId) {
@@ -215,6 +278,17 @@ public class MainActivity extends Activity {
 	    editor.commit();
 	}
 	
+	private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+ 
+        inputStream.close();
+        return result;
+ 
+    }
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
